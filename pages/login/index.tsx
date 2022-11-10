cf300de502e3a02740a494dc5eaab497dc6662bd
Peter <peter@cacdi.com>
2022-11-10 12:28:26 +0800

    Linked 登入流程 => 取得使用者同意授權

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,25 @@
+import queryString from 'query-string'
+
+const CLIENT_ID = '86mpic7af5qo71'
+const CLIENT_SECRET = 'l1IKqVoXwd5FYGRg'
+const REDIRECT_URI = 'next-deploy-demo-product.vercel.app/login'
+const SCOPE = 'r_liteprofile%20r_emailaddress'
+
 const LoginPage = () => {
+  const _LinkedInSignIn = async () => {
+    // 取得 authorization code
+    const queryAUCode = {
+      response_type: 'code',
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      scope: SCOPE,
+    }
+    fetch(`https://www.linkedin.com/oauth/v2/authorization?${queryString.stringify(queryAUCode)}`)
+  }
   return (
-    <button>LinkedIn 登入</button>
+    <button onCkick={_LinkedInSignIn}>LinkedIn 登入</button>
   );
 };
+
 
 export default LoginPage;
