6cd24d425b18fc962d8c82e9f8dfdf51526dba00
Peter <peter@cacdi.com>
2022-11-11 14:28:57 +0800

    add google site vertification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Head>
-      <meta name="google-site-verification" content="WNq3IRTEqjmhLO_unhaH8uAa8n_6UIki-8fuP9EBgw8" />
-    </Head> 
-    <Component {...pageProps} />;
+    <>
+      <Head>
+        <meta name="google-site-verification" content="WNq3IRTEqjmhLO_unhaH8uAa8n_6UIki-8fuP9EBgw8" />
+      </Head> 
+      <Component {...pageProps} />
+    </>
   )
 }
 
