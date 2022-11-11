dc972cc3c2c29e332f06552343b711bab331565e
Peter <peter@cacdi.com>
2022-11-11 14:23:21 +0800

    add google site vertification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Head>
+      <meta name="google-site-verification" content="WNq3IRTEqjmhLO_unhaH8uAa8n_6UIki-8fuP9EBgw8" />
+    </Head> 
+    <Component {...pageProps} />;
+  )
 }
 
 export default MyApp;
