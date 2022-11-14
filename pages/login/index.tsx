d822a89b04b4f8fda5c7e46e2c2a57eb1a23eaaa
Peter <peter@cacdi.com>
2022-11-14 11:18:48 +0800

    測試 google 使用者資料

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -115,7 +115,7 @@ const LoginPage = () => {
       {/* Google 登入點擊導轉到登入畫面 */}
       <GoogleLogin
         onSuccess={credentialResponse => {
-          console.log(credentialResponse);
+          console.log(JSON.stringify(credentialResponse.profileObj));
           router.push('/products')
         }}
         onError={() => {
